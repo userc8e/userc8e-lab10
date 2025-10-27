62deed606d9e6591d171635fcb8a63e90ba09506
userc8e <caitlynquack@gmail.com>
2025-10-27 11:01:42 -0400

    initialized methods in WordCounter.java

--- WordCounter.java
+++ WordCounter.java
@@ -1,3 +1,24 @@
 public class WordCounter {
-    
+
+    //  METHODS
+
+    // counts number of words in text through stopword
+    // if stopword null --> count all words in file
+    // return int count; unless count < 5 --> TooSmallText exception
+    public int processText(StringBuffer text, String stopword) {
+        return -1; //placeholder
+    }
+
+    // converts path to a StringBuffer
+    // returns StringBuffer
+    // if file cant open --> prompt user to re-enter filenanme until it can open
+    // if empty file --> raise EmptyFileException w/ file path in msg
+    public StringBuffer processFile(String path) {
+        return null; //placeholder
+    }
+
+    // main method
+    public static void main(String[] args) {
+        
+    }
 }
