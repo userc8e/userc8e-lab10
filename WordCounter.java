7d417dedcf25dd27a2b6730efd45d12e5f9d03fe
userc8e <caitlynquack@gmail.com>
2025-11-02 22:50:36 -0500

    implemented processText()

--- WordCounter.java
+++ WordCounter.java
@@ -9,19 +9,52 @@ public class WordCounter {
     // if stopword null --> count all words in file
     // return int count; unless count < 5 --> TooSmallText exception
     public int processText(StringBuffer text, String stopword) throws InvalidStopWordException, TooSmallText {
+        boolean stopwordFound = false;
+        int wordCount = 0; //words in text through stopword
+        int fullCount = 0; /*count of all words in text
+                            will be returned if stopword is null*/
+
         if (text == null) {
             return 0;
         }
 
         int count = 0;
-        Pattern regex = Pattern.compile("\\w");
+        Pattern regex = Pattern.compile("[a-zA-Z0-9']+");
         Matcher regexMatcher = regex.matcher(text);
         while (regexMatcher.find()) {
-            System.out.println("I just found the word: " + regexMatcher.group());
-        } 
-        
+            //System.out.println("I just found the word: " + regexMatcher.group());
+            
+            String word = regexMatcher.group();
 
-        return -1; //placeholder
+            if (word.equals(stopword)) {
+                stopwordFound = true;
+            }
+
+            if (stopword.equals("")) {
+                fullCount++;
+            } else {
+                wordCount++;
+            }
+        } 
+
+        // if the stopword is null, count all words in the file
+        if (stopword.equals("")) {
+            // method returns integer word count
+            if (fullCount >= 5) {
+                return fullCount;
+            
+            // unless word count <5 - throw TooSmallText exception
+            } else {
+                throw new TooSmallText("Text has " + fullCount + " words; need at least 5 words.");
+            }
+        }
+
+        // if stopword is not found
+        if (stopwordFound == false) {
+            throw new InvalidStopWordException("Stopword was not found in text.");
+        }
+        
+        return wordCount;
     }
 
     // converts path to a StringBuffer
